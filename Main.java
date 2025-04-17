4ae909018641328688506d808ac183fd314d1f5c
Lunivaa <np03cs4s240007@heraldcollege.edu.np>
2025-04-17 14:42:29 +0545

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main { 
-    public static void main(String[] args) { 
-        Student student1 = new Student("John Doe", 20); 
-        System.out.println("Student Name: " + student1.getName()); 
-        System.out.println("Student Age: " + student1.getAge()); 
-    } 
-}
